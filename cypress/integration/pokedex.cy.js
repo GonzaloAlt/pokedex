d2cc3e06a375ecd8669852f7f21266e3c37043af
Gonzalo Altamirano <gonza12683@gmail.com>
2022-07-14 15:26:03 -0300

    Cypress tests

--- cypress/integration/pokedex.cy.js
+++
@@ -1,7 +0,0 @@
-// const URL = 'http://127.0.0.1:8080';
-
-context('Pokedex UI', () => {
-  it('se asegura que se hayan renderizado los pokemones', () => {
-    cy.get('#pokemon-list-btns').find('.pokemon-btn').should('have.length', 20);
-  });
-});
